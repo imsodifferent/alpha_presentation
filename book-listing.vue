950ab4272cf864cd5677f42911e001af97ece71f
son of cain <nika.sanikidze234@gmail.com>
2025-02-01 13:38:46 +0400

    Create book-listing.vue

---
+++ book-listing.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { useFilterStore } from '@/stores/FilterStore';
+import BookCard from '@/components/common/BookCard.vue';
+import { storeToRefs } from 'pinia';
+
+const { filteredBooks, isAnyFilterSet, books } = storeToRefs(useFilterStore());
+</script>
+
+<template>
+	<div class="mt-10 flex gap-8 flex-wrap items-center">
+		<BookCard v-for="book in isAnyFilterSet ? filteredBooks : books" :key="book?.id || book?.slug" :book="book" />
+	</div>
+</template>
